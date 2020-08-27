e0e70bee92917232cb64c7550ec3b1bb6bb3f308
sergaik-web <seroga8863@gmail.com>
2020-08-27 22:50:26 +0300

    feat: create example in App

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Hoc from "../Hoc/Hoc";
 import './App.scss';
 
-const App = () => {
+const App = ({service}:any) => {
+    // service.testMethodPost("http://localhost:3004/users", { username: 'example' })
+    //     .then((res: any)=>console.log(res));
+    //
+    // service.testMethodGet("http://localhost:3004/users")
+    //     .then((res:any)=>console.log(res));
+
   return (
     <div className="App">
       APP
