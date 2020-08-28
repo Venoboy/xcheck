28e32c1d857da63957a637254b6048fd466c344f
Anhelina Zhurauliova <anhelinazhurauliova@gmail.com>
2020-08-28 19:40:55 +0300

    feat: add eslint and prettier config

---
+++ .eslintrc.js
@@ -0,0 +1,62 @@
+module.exports = {
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  extends: [
+    'airbnb',
+    'prettier',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:react/recommended',
+  ],
+  plugins: ['babel', 'import', 'jsx-a11y', 'react', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'linebreak-style': 'off',
+
+    'arrow-parens': 'off',
+    'object-curly-newline': 'off',
+    'no-mixed-operators': 'off',
+    'function-paren-newline': 'off',
+    'no-plusplus': 'off',
+    'space-before-function-paren': 0,
+    'import/prefer-default-export': 'off',
+    'no-underscore-dangle': ['error', { allow: ['id_', '_id'] }],
+    'max-len': ['error', 100, 2, { ignoreUrls: true }],
+    'no-console': 'error',
+    'no-alert': 'error',
+
+    'no-param-reassign': 'off',
+    radix: 'off',
+
+    'react/require-default-props': 'off',
+    'react/forbid-prop-types': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/jsx-filename-extension': ['error', { extensions: ['.js'] }],
+
+    'prefer-destructuring': 'off',
+
+    'jsx-a11y/anchor-is-valid': ['error', { components: ['Link'], specialLink: ['to'] }],
+
+    'jsx-a11y/label-has-associated-control': [
+      2,
+      {
+        labelComponents: [],
+        labelAttributes: [],
+        controlComponents: [],
+        assert: 'either',
+        depth: 3,
+      },
+    ],
+
+    'prettier/prettier': ['error'],
+  },
+};
