52b4dcee4cb3066ff0825a23562e50f36b5a8d12
Anhelina Zhurauliova <56220481+anhelina-zhurauliova@users.noreply.github.com>
2020-08-30 13:17:16 +0300

    Feat/configs (#3)
    * feat: add eslint and prettier config
    
    * fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App/App';
-import ErrorBoundary from "./Components/ErrorBoundary/ErrorBoundary";
-import Store from "./Store/Store";
-import Context from "./Context/Context";
-import Service from "./Service/Service";
-import { Provider } from "react-redux";
+import ErrorBoundary from './Components/ErrorBoundary/ErrorBoundary';
+import Store from './Store/Store';
+import Context from './Context/Context';
+import Service from './Service/Service';
+import { Provider } from 'react-redux';
 import './index.scss';
 
 const service: any = new Service();
 
 ReactDOM.render(
-    <Provider store={Store}>
-        <ErrorBoundary>
-            <Context.Provider value={service}>
-                <App />
-            </Context.Provider>
-        </ErrorBoundary>
-    </Provider>,
-  document.getElementById('root')
+  <Provider store={Store}>
+    <ErrorBoundary>
+      <Context.Provider value={service}>
+        <App />
+      </Context.Provider>
+    </ErrorBoundary>
+  </Provider>,
+  document.getElementById('root'),
 );
