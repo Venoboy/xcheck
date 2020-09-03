816923e6853cf571f44a65b29990f524a5e70fc7
sergaik-web <seroga8863@gmail.com>
2020-09-03 15:36:29 +0200

    feat: add RangeDate in TaskForm

--- src/Components/TaskForm/TaskForm.tsx
+++ src/Components/TaskForm/TaskForm.tsx
@@ -1,45 +1,42 @@
 import React from 'react';
-import { Input, Form, Row, Col } from 'antd';
+import { Input, Form, DatePicker } from 'antd';
 import { EditOutlined } from '@ant-design/icons';
 import './TaskForm.scss';
 import 'antd/dist/antd.css';
 
 const TaskForm = () => {
+  const { RangePicker } = DatePicker;
   const onFinish = (values: string) => {
     console.log('Success:', values);
   };
   const onFinishFailed = (errorInfo: any) => {
     console.log('Failed:', errorInfo);
   };
+  // @ts-ignore
+  // @ts-ignore
   return (
     <Form
       id="task-form"
+      labelCol={{ span: 2 }}
+      wrapperCol={{ span: 10 }}
       name="basic"
       initialValues={{ remember: true }}
       onFinish={onFinish}
       onFinishFailed={onFinishFailed}
     >
-      <Row>
-        <Col span={10}>
-          <Form.Item
-            label="Task Name"
-            name="task-name"
-            rules={[{ required: true, message: 'Please enter Task Name' }]}
-          >
-            <Input placeholder="Enter Task Name" suffix={<EditOutlined />} />
-          </Form.Item>
-        </Col>
-        <Col span={6}>
-          <Form.Item
-            label="Author"
-            name="author-name"
-            rules={[{ required: true }]}
-            initialValue="User"
-          >
-            <Input placeholder="Author" suffix={<EditOutlined />} />
-          </Form.Item>
-        </Col>
-      </Row>
+      <Form.Item
+        label="Task Name"
+        name="task-name"
+        rules={[{ required: true, message: 'Please enter Task Name' }]}
+      >
+        <Input placeholder="Enter Task Name" suffix={<EditOutlined />} />
+      </Form.Item>
+      <Form.Item label="Author" name="author-name" rules={[{ required: true }]} initialValue="User">
+        <Input placeholder="Author" suffix={<EditOutlined />} />
+      </Form.Item>
+      <Form.Item label="Range Date" name="deadline-time" rules={[{ required: true }]}>
+        <RangePicker showTime placeholder={['Start Date', 'Deadline']} />
+      </Form.Item>
     </Form>
   );
 };
