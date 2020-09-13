7bd78ec386818ed020cfd5beeb0cb107b1bfe279
stanislavvitvitskiy <stanislav.i.vitvitskiy@gmail.com>
2020-09-13 09:37:09 -0700

    refactor: move router to the app

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import './App.scss';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Dispute from '../Dispute/Dispute';
+import { TasksList } from '../TasksList/TasksList';
+import { TaskReview } from '../../TaskReview/TaskReview';
 
-const App: React.FC = ({ children }) => {
-  return <div className="App">{children}</div>;
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/dispute">
+            <Dispute isActive={false} />
+          </Route>
+          <Route path="/tasks-list">
+            <TasksList />
+          </Route>
+          <Route path="/task-review">
+            <TaskReview />
+          </Route>
+        </Switch>
+      </Router>
+    </div>);
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ErrorBoundary from './Components/ErrorBoundary/ErrorBoundary';
 import Store from './Store/Store';
 import Context from './Context/Context';
 import Service from './Service/Service';
 import './index.scss';
 import App from './Components/App/App';
-import Dispute from './Components/Dispute/Dispute';
-import { TasksList } from './Components/TasksList/TasksList';
-import { TaskReview } from './TaskReview/TaskReview';
 
 const service = new Service();
 
@@ -18,25 +14,7 @@ ReactDOM.render(
   <Provider store={Store}>
     <ErrorBoundary>
       <Context.Provider value={service}>
-        <Router>
-          <Switch>
-            <Route path="/dispute">
-              <App>
-                <Dispute isActive={false} />
-              </App>
-            </Route>
-            <Route path="/tasks-list">
-              <App>
-                <TasksList />
-              </App>
-            </Route>
-            <Route path="/task-review">
-              <App>
-                <TaskReview />
-              </App>
-            </Route>
-          </Switch>
-        </Router>
+        <App />
       </Context.Provider>
     </ErrorBoundary>
   </Provider>,
