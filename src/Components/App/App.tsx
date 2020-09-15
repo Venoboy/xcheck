b0d575c66bd3000180509b9d6aac567b85f4e211
Anhelina Zhurauliova <anhelinazhurauliova@gmail.com>
2020-09-15 15:31:45 +0300

    fix: fix merge conflicts

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import TasksCreator from '../TasksCreator/TasksCreator';
+import { Selfcheck } from '../Selfcheck/Selfcheck';
 import 'antd/dist/antd.css';
 import './App.scss';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Dispute from '../Dispute/Dispute';
 import { TasksList } from '../TasksList/TasksList';
 import { TaskReview } from '../../TaskReview/TaskReview';
@@ -19,6 +21,12 @@ const App: React.FC = () => {
           </Route>
           <Route path="/task-review">
             <TaskReview />
+          </Route>
+          <Route path="/selfcheck">
+            <Selfcheck />
+          </Route>
+          <Route path="/tasks-creator">
+            <TasksCreator />
           </Route>
         </Switch>
       </Router>
