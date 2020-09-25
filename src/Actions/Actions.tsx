8643fee3f28b81043215590823889cf1ed515dc5
Vladislav Ivanov <himametsu@gmail.com>
2020-09-25 18:06:41 +0300

    feat: updated action

--- src/Actions/Actions.tsx
+++ src/Actions/Actions.tsx
@@ -5,8 +5,13 @@ const requests = (): requestsType => {
   };
 };
 
-export type TaskInfo = { selectedTaskId: string; checkSessionId: string };
-const changeSelectedTaskId = (taskInfo: TaskInfo) => {
+export type TaskInfoPayload = { selectedTaskId: string; checkSessionId: string };
+export interface SelectedTaskAction {
+  type: 'CHANGE_SELECTED_TASK_INFO';
+  payload: TaskInfoPayload;
+}
+
+const changeSelectedTaskId = (taskInfo: TaskInfoPayload): SelectedTaskAction => {
   const { selectedTaskId, checkSessionId } = taskInfo;
   return {
     type: 'CHANGE_SELECTED_TASK_INFO',
